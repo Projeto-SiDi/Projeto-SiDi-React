e6d9c7df43a77d157d3d2a02f944a506461a1556
Vynicius <vynicius.henrique15@gmail.com>
2025-05-06 16:55:07 -0300

    criando layout para o form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid2';
+import Stack from '@mui/material/Stack';
+
 export function App() {
 
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: '#303741',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    alignContent: 'center',
+    flexDirection: "column",
+  
+  }));
+
+  
+  
 
   return (
 
-      <div>
-
-      </div>
+    <Box  sx={{ pl: 2 }}>
+    <Grid container spacing={4} sx={{ height: "800px", justifyContent: "center", alignItems: "center", marginTop: "70px" }} >
+      <Grid size={4}>
+        <Stack spacing={2}>
+          <Item>Forumlario</Item>
+        </Stack>
+      </Grid>
+    </Grid>
+  </Box>
   )
+
 }
 
 
