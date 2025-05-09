a12ebfe3c98aa37112b96655cfba44abdfae7f6f
Vynicius <vynicius.henrique15@gmail.com>
2025-05-08 23:23:48 -0300

    criando layout para o form

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid2';
 import Stack from '@mui/material/Stack';
+import { useForm } from "react-hook-form";
+import TextField from '@mui/material/TextField';
+import { FormControl, InputLabel, OutlinedInput, InputAdornment, IconButton, Button, withStyles} from '@mui/material';
+import { Eye, EyeClosed } from '@phosphor-icons/react';
+import logoSidi from './assets/logoSidi.png';
+
 
 export function App() {
 
@@ -17,16 +23,70 @@ export function App() {
   
   }));
 
+  const BuuttonStyle = {
+    background: '#00ED3C'
+  }
+
   
-  
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+  };
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
 
   return (
 
-    <Box  sx={{ pl: 2 }}>
-    <Grid container spacing={4} sx={{ height: "800px", justifyContent: "center", alignItems: "center", marginTop: "70px" }} >
-      <Grid size={4}>
-        <Stack spacing={2}>
-          <Item>Forumlario</Item>
+    <Box sx={{ display: 'flex',  alignItems: 'center', minWidth: '30%', pl: '32%' }}>
+    <Grid sx={{ height: "800px", justifyContent: "center", alignItems: 'center', width: '900px', flexDirection: 'column'}} >
+      <Grid sx={{maxWidth: '80%'}}>
+        <Stack>
+          <Item>
+            <div>
+              
+              
+                <TextField
+                id="outlined-multiline-flexible"
+                label="Email"
+                maxRows={2}
+                sx={{height: '56px', width: '38rem', marginTop: '30%', marginBottom: '30px'}}
+                
+              />
+              <FormControl sx={{ m: 1,width: '38rem', height: '56px'}} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={showPassword ? 'text' : 'password'}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label={
+                          showPassword ? 'hide the password' : 'display the password'
+                        }
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        onMouseUp={handleMouseUpPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <EyeClosed/> : <Eye/>}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+                <Button style={BuuttonStyle} variant='contained' sx={{marginTop: '20px'}}>Entrar</Button>
+              </FormControl>
+              
+            </div>
+            
+          </Item>
         </Stack>
       </Grid>
     </Grid>
@@ -34,5 +94,7 @@ export function App() {
   )
 
 }
+
+
 
 
